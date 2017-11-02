a750e780739f9d5d458fa7c47db53d350407dc91
idgm81 <ivan.dgracia@gmail.com>
2017-11-02 20:59:06 +0100

    fixed scaffoldin

--- app/adapters/application.js
+++
@@ -1,7 +0,0 @@
-import JSONAPIAdapter from 'ember-data/adapters/json-api';
-
-export default JSONAPIAdapter.extend({
-    namespace: 'api',
-    host: 'https://localhost:4500',
-    headers: {"Content-Type":"application/json"}
-});
