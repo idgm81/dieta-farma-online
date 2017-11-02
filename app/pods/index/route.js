409706a9945fec1b709875f7f911ce110c06314c
idgm81 <ivan.dgracia@gmail.com>
2017-11-02 21:00:25 +0100

    added click button transitions

--- app/pods/index/route.js
+++ app/pods/index/route.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
+import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(UnauthenticatedRouteMixin, {
+
+  actions: {
+    goToLogin() {
+      this.transitionTo('login');
+    },
+
+    goToRegister() {
+      this.transitionTo('register');
+    }
+  }
 });
