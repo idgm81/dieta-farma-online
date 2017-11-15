020719d87d951387a3a69d61d41082d1b1292e5e
idgm81 <ivan.dgracia@gmail.com>
2017-11-15 22:12:54 +0100

    added profile layout

--- app/pods/home/profile/route.js
+++ app/pods/home/profile/route.js
@@ -3,5 +3,11 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
 
+  model() {
+    const id = this.get('session.data.authenticated.id');
+
+    return this.store.findRecord('user', id);
+  },
+
   actions: {}
 });
