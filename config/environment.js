2be2957be1d80a0536d4ebc0d7127d76d65215f9
idgm81 <ivan.dgracia@gmail.com>
2017-11-15 20:09:57 +0100

    set api endpoint

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,7 @@
 /* eslint-env node */
 
 module.exports = function(environment) {
+  var HOST = environment === 'production' ? 'dieta-farma-api.herokuapp.com' : 'localhost';
   var ENV = {
     modulePrefix: 'dieta-farma-online',
     podModulePrefix: 'dieta-farma-online/pods',
@@ -21,12 +22,13 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+      API_HOST: `http://${HOST}:4500`
     },
     contentSecurityPolicy: {
       'default-src': "'none'",
       'script-src': "'self' 'unsafe-inline' 'unsafe-eval'",
       'font-src': "'self'",
-      'connect-src': "'self'",
+      'connect-src': "'self' http://localhost:4500",
       'img-src': "'self'",
       'report-uri':"'localhost'",
       'style-src': "'self' 'unsafe-inline'",
@@ -35,20 +37,21 @@ module.exports = function(environment) {
   };
 
   ENV['ember-simple-auth'] = {
-    authorizer: 'authorizer:jwt'
+    authorizer: 'authorizer:jwt',
+    routeAfterAuthentication: 'home'
   };
 
+
   ENV['ember-simple-auth-token'] = {
-    serverTokenEndpoint: '/api/token-auth/',
+    serverTokenEndpoint: `http://${HOST}:4500/api/auth/user`,
     tokenPropertyName: 'token',
     identificationField: 'email',
     passwordField: 'password',
-    refreshAccessTokens: true,
+    refreshAccessTokens: false,
     authorizationPrefix: 'JWT ',
-    authorizationHeaderName: 'x-auth-token',
     refreshTokenPropertyName: 'refresh_token',
     refreshLeeway: 300, // Refresh the token 5 minutes (300s) before it expires.,
-    serverTokenRefreshEndpoint: '/api/token-refresh/',
+    serverTokenRefreshEndpoint: `http://${HOST}:4500/api/auth/refresh_token`,
     tokenExpireName: 'exp'
   };
 
