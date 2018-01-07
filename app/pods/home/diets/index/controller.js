9f69479dbfd388baaf286eb2cecc10009b05c501
idgm81 <ivan.dgracia@gmail.com>
2018-01-07 19:02:31 +0100

    delete diet modal

--- app/pods/home/diets/index/controller.js
+++ app/pods/home/diets/index/controller.js
@@ -9,8 +9,6 @@ export default Ember.Controller.extend({
 
   api: service(),
 
-  flashMessages: service(),
-
   isNutriotionist: computed.equal('session.data.authenticated.role', USER_ROLES.NUTRITIONIST),
 
   actions: {
@@ -19,14 +17,17 @@ export default Ember.Controller.extend({
     },
 
     delete(id) {
+      $('#modal-wait-delete-diet').modal();
       return this.get('api').deleteDiet(id)
         .then(() => {
-          this.get('flashMessages').success('Dieta eliminada!');
           this.replaceRoute('home.diets.index');
         })
         .catch(() => {
-          this.get('flashMessages').error('Error al eliminar la dieta!');
-        });
+          $('#modal-delete-diet-error').modal();
+        })
+        .finally(() => {
+          $('#modal-wait-delete-diet').modal('hide');
+        })
     }
   }
 });
