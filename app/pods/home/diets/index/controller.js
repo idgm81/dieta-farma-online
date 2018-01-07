ca273a0a6b5cd30e16bd2630e18a26102c95f578
idgm81 <ivan.dgracia@gmail.com>
2018-01-07 19:22:19 +0100

    fixed nutritionist deletes diet

--- app/pods/home/diets/index/controller.js
+++ app/pods/home/diets/index/controller.js
@@ -7,27 +7,5 @@ export default Ember.Controller.extend({
 
   session: service(),
 
-  api: service(),
-
   isNutriotionist: computed.equal('session.data.authenticated.role', USER_ROLES.NUTRITIONIST),
-
-  actions: {
-    open(url) {
-      window.open(url);
-    },
-
-    delete(id) {
-      $('#modal-wait-delete-diet').modal();
-      return this.get('api').deleteDiet(id)
-        .then(() => {
-          this.replaceRoute('home.diets.index');
-        })
-        .catch(() => {
-          $('#modal-delete-diet-error').modal();
-        })
-        .finally(() => {
-          $('#modal-wait-delete-diet').modal('hide');
-        })
-    }
-  }
 });
--- app/pods/home/diets/index/route.js
+++ app/pods/home/diets/index/route.js
@@ -13,5 +13,25 @@ export default Route.extend(AuthenticatedRouteMixin, {
     const userId = transition.queryParams.userId || this.get('session.data.authenticated.id');
 
     return this.get('api').getDiets(userId);
+  },
+
+  actions: {
+    open(url) {
+      window.open(url);
+    },
+
+    delete(id) {
+      $('#modal-wait-delete-diet').modal();
+      return this.get('api').deleteDiet(id)
+        .then(() => {
+          this.refresh();
+        })
+        .catch(() => {
+          $('#modal-delete-diet-error').modal();
+        })
+        .finally(() => {
+          $('#modal-wait-delete-diet').modal('hide');
+        })
+    }
   }
 });
