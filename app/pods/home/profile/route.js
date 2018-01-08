eb0c2aa9c21d614df8e32a344437e7e7535b2fcd
idgm81 <ivan.dgracia@gmail.com>
2018-01-08 12:17:17 +0100

    added delete user action

--- app/pods/home/profile/route.js
+++ app/pods/home/profile/route.js
@@ -4,6 +4,8 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 const { Route, inject: { service } } = Ember;
 export default Route.extend(AuthenticatedRouteMixin, {
 
+  session: service(),
+
   api: service(),
 
   model(params) {
@@ -11,11 +13,14 @@ export default Route.extend(AuthenticatedRouteMixin, {
   },
 
   actions: {
-    showDiets() {
-      this.transitionTo('home.diets');
+    delete(id) {
+      this.get('api').deleteUser(id).then(() => this.get('session').invalidate())
     },
     edit(id) {
       this.transitionTo('home.profile.edit', id);
-    }
+    },
+    showDiets() {
+      this.transitionTo('home.diets');
+    },
   }
 });
