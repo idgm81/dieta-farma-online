fb8e20b106b25e77eea8e64a4a957ff241ca3bff
idgm81 <ivan.dgracia@gmail.com>
2018-01-08 18:49:11 +0100

    new public text

--- app/locales/es/translations.js
+++ app/locales/es/translations.js
@@ -8,6 +8,35 @@ export default {
   },
   "text": {
     "accessing": "Accediendo...",
+    "public": {
+      "aboutMe": {
+        "title": "\"Soy farmacéutico de profesión, nutricionista por vocación, y entrenador por devoción\"",
+        "message": "Realizo consulta dietética para ayudar a las personas a lograr su objetivo. Ahora lo hago online para llegar más lejos, a más personas, con un mensaje más claro, pero sin perder ni una gota de calidad respecto al consejo dietético presencial que ya llevo ejerciendo una larga temporada.<br>Si tú te propones firmemente cambiar tus hábitos dietéticos, juntos lo conseguiremos. Me seguiré forman continuamente para permanecer a la vanguardia de la dietética, pero siempre con rigor científico y sin milagros."
+      },
+      "banner": {
+        "title": "PONTE EN FORMA CON DIETAFARMA",
+        "message": "Conscientes de que el tiempo es ORO, Dietafarma te ofrece un servicio de dietética 100% online.<br>Sólo necesitas registrarte y contestar a nuestro cuestionario para obtener tu dieta personalizada o si lo prefieres puedes solicitar una cita para que sea a través de Skype o de forma presencial."
+      },
+      "buttons": {
+        "register": "Regístrate",
+        "signin": "Identifícate"
+      },
+      "footer": {
+        "copyright": "© Dietafarma. All Rights Reserved"
+      },
+      "nav": {
+        "home": "INICIO",
+        "services": "SERVICIOS",
+        "about": "ACERCA DE",
+        "contact": "CONTACTA"
+      },
+      "services": {
+        "paragraph1": "Contesta en unos minutos nuestro formulario definido por el experto Jorge Baztán (farmacéutico, nutricionista y entrenador personal) y obtén una dieta que se ajusta a tus necesidades",
+        "paragraph2": "Resuelve cualquier duda directamente con tu nutricionista a través de nuestro servicio de 'Mensajes' de la aplicación",
+        "paragraph3": "Realizaremos un seguimiento continuo de tu dieta y podrás renovarla periódicamente",
+        "paragraph4": "Si te gusta más el contacto directo reserva tu consulta de Skype o presencial a través del servicio de 'Citas' de la aplicación"
+      }
+    },
     "signUp": "Registrando, por favor espere..."
   }
 };
