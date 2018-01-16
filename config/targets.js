f6c7cbfc896a6428cbd8d1bfd61d248e6e0ef702
idgm81 <ivan.dgracia@gmail.com>
2018-01-16 22:56:20 +0100

    supported browsers

--- config/targets.js
+++ config/targets.js
@@ -2,9 +2,10 @@
 
 module.exports = {
   browsers: [
-    'ie 9',
-    'last 1 Chrome versions',
-    'last 1 Firefox versions',
-    'last 1 Safari versions'
+    'ie 11',
+    'last 6 chrome versions',
+    'last 3 firefox versions',
+    'last 3 safari versions',
+    'last 1 edge versions'
   ]
 };
