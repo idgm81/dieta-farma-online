3156cebc1c40cfcdccb07d9a7f94341b0a3d86b8
idgm81 <ivan.dgracia@gmail.com>
2018-02-09 20:21:26 +0100

    show inbox messages number

--- app/pods/home/route.js
+++ app/pods/home/route.js
@@ -3,7 +3,7 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 import ENV from '../../config/environment';
 import { USER_ROLES } from './constants';
 
-const { inject: { service } , computed, get, run} = Ember;
+const { inject: { service } , computed, get, run, RSVP} = Ember;
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
 
@@ -16,9 +16,12 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
   userId: computed.reads('session.data.authenticated.id'),
 
   model() {
-    const userId = this.get('session.data.authenticated.id');
+    const id = get(this, 'userId');
 
-    return this.get('api').getUser(userId);
+    return RSVP.hash({
+      userData: this.get('api').getUser(id),
+      messagesData: this.get('api').getMessages(id)
+    });
   },
 
   redirect(model) {
@@ -26,7 +29,7 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
 
     run.later(() => this.get('session').invalidate(), 10 * 60 * 1000); //close session after 10 minutes
 
-    const nextRoute = get(model, 'user.role') === USER_ROLES.NUTRITIONIST
+    const nextRoute = get(model, 'userData.user.role') === USER_ROLES.NUTRITIONIST
       ? 'home.clients.index'
       : 'home.diets';
 
@@ -34,11 +37,11 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
   },
 
   setupController: function(controller, model) {
-    controller.set('isClient', get(model, 'user.role') === USER_ROLES.CLIENT);
-    controller.set('headerTitle', `Hola ${get(model, 'user.profile.name')}`);
-    controller.set('fullName', `${get(model, 'user.profile.name')} ${get(model, 'user.profile.surname')}`);
+    controller.set('isClient', get(model, 'userData.user.role') === USER_ROLES.CLIENT);
+    controller.set('headerTitle', `Hola ${get(model, 'userData.user.profile.name')}`);
+    controller.set('fullName', `${get(model, 'userData.user.profile.name')} ${get(model, 'userData.user.profile.surname')}`);
     controller.set('appVersion', `${ENV.APP.version}.${ENV.APP.buildDate}`);
-    //controller.set('unreadMessages', model.messages.getEach('_id').length);
+    controller.set('inboxMessages', get(model, 'messagesData.messages').length);
   },
 
   close() {
@@ -48,7 +51,7 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
   actions: {
     showMyProfile() {
       this.close();
-      this.transitionTo('home.profile.index', this.get('userId'));
+      this.transitionTo('home.profile.index', get(this, 'userId'));
     },
 
     askForAppointment() {
