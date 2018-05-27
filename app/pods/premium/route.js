a20b5d0310b24d2aa8f9e2a047e7ab5371f0cc9c
idgm81 <ivan.dgracia@gmail.com>
2018-05-27 21:16:06 +0200

    added new premium icons

--- app/pods/premium/route.js
+++ app/pods/premium/route.js
@@ -1,8 +1,16 @@
 import Route from '@ember/routing/route';
+import { reads }  from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 
 export default Route.extend({
+
+  session: service(),
+
+  userId: reads('session.data.authenticated.id'),
+
   actions: {
-    buy() {}
+    buy(price) {
+      this.transitionTo('payment', { queryParams: { price } });
+    }
   }
 });
