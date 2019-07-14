b7348dd05fc236503c06fb579bde4b53f8ac9dc4
idgm81 <ivan.dgracia@gmail.com>
2019-07-14 12:47:01 +0200

    update targets

--- config/targets.js
+++ config/targets.js
@@ -1,12 +1,18 @@
 /* eslint-env node */
+const browsers = [
+  'last 3 chrome versions',
+  'last 3 firefox versions',
+  'last 2 safari versions',
+  'last 3 iOS versions',
+  'last 2 edge versions'
+];
+const isCI = !!process.env.CI;
+const isProduction = process.env.EMBER_ENV === 'production';
+
+if (isCI || isProduction) {
+  browsers.push('ie 11');
+}
 
 module.exports = {
-  browsers: [
-    'last 3 ie versions',
-    'last 6 chrome versions',
-    'last 6 firefox versions',
-    'last 2 safari versions',
-    'last 4 iOS versions',
-    'last 2 edge versions'
-  ]
+  browsers
 };
