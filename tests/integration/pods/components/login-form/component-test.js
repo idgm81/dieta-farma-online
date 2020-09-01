476850aa01a4e68d0be449a1ba6ef7cf519371c1
idgm81 <ivan.dgracia@gmail.com>
2020-09-01 14:55:12 +0200

    update dependencies

--- tests/integration/pods/components/login-form/component-test.js
+++ tests/integration/pods/components/login-form/component-test.js
@@ -1,24 +1,26 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('login-form', 'Integration | Component | login form', {
-  integration: true
-});
+module('Integration | Component | login form', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{login-form}}`);
+    await render(hbs`{{login-form}}`);
 
-  assert.equal(this.$().text().trim(), '');
+    assert.dom('*').hasText('');
 
-  // Template block usage:
-  this.render(hbs`
-    {{#login-form}}
-      template block text
-    {{/login-form}}
-  `);
+    // Template block usage:
+    await render(hbs`
+      {{#login-form}}
+        template block text
+      {{/login-form}}
+    `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+    assert.dom('*').hasText('template block text');
+  });
 });
