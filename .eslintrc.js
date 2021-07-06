fe45d6cd3b5893f46d15ee1ad25fd58ba61efd60
idgm81 <ivan.dgracia@gmail.com>
2021-07-06 21:02:59 +0200

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,45 @@
 module.exports = {
   root: true,
+  parser: 'babel-eslint',
   parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module'
-  },
-  extends: 'eslint:recommended',
+		ecmaVersion: 2018,
+		sourceType: 'module',
+		ecmaFeatures: {
+			legacyDecorators: true
+		}
+	},
+  plugins: ['ember'],
+  extends: [
+    'eslint:recommended',
+    'plugin:ember/recommended' // or other configuration
+  ],
   env: {
     browser: true,
     jquery: true
   },
   rules: {
-    "no-unused-vars": [ "warn" ]
+    'ember/avoid-leaking-state-in-ember-objects': 1,
+		'ember/closure-actions': 1,
+		'ember/jquery-ember-run': 0,
+		'ember/no-arrow-function-computed-properties': 0,
+		'ember/no-deeply-nested-dependent-keys-with-each': 0,
+		'ember/no-duplicate-dependent-keys': 0,
+		'ember/no-global-jquery': 0,
+		'ember/no-incorrect-calls-with-inline-anonymous-functions': 0,
+		'ember/no-mixins': 1,
+		'ember/no-new-mixins': 1,
+		'ember/require-computed-property-dependencies': 1,
+		'ember/no-private-routing-service': 1,
+		'ember/no-observers': 1,
+		'ember/no-on-calls-in-components': 0,
+		'ember/no-side-effects': 1,
+		'ember/no-get': 0,
+		'ember/no-get-with-default': 1,
+		'ember/no-jquery': 1,
+		'ember/routes-segments-snake-case': 0,
+		'ember/use-brace-expansion': 0,
+		'ember/use-ember-get-and-set': 1,
+    "no-unused-vars": 2
   },
   overrides: [
     // node files
@@ -26,21 +55,24 @@ module.exports = {
         'server/**/*.js'
       ],
       parserOptions: {
-        sourceType: 'script',
-        ecmaVersion: 2015
-      },
+				sourceType: 'module',
+				ecmaVersion: 2018
+			},
       env: {
         browser: false,
         node: true
       },
       plugins: ['node'],
-      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
-        // add your custom rules and overrides for node files here
-
-        // this can be removed once the following is fixed
-        // https://github.com/mysticatea/eslint-plugin-node/issues/77
-        'node/no-unpublished-require': 'off'
-      })
+      extends: ['plugin:node/recommended'],
+      rules: {
+				'no-sync': 0,
+				'no-process-env': 0,
+				'node/shebang': 0,
+				'node/no-extraneous-require': 1,
+				// this can be removed once the following is fixed
+				// https://github.com/mysticatea/eslint-plugin-node/issues/77
+				'node/no-unpublished-require': 'off'
+			}
     }
   ]
 };
